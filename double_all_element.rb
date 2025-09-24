37a1ba64a443bd30533b1a8c0b03ca9d7b176ff7
Sanskar <sarathore@grepruby.io>
2025-09-24 13:39:27 +0530

    update double element code

--- double_all_element.rb
+++ double_all_element.rb
@@ -1,7 +1,22 @@
+# def double_element(arr)
+#     result = arr.map {|i| i * 2}
+#     p result
+# end
+
+# arr =[1,2,3,4,5,]
+# double_element(arr)
+
 def double_element(arr)
-    result = arr.map {|i| i * 2}
+    result = arr.map do |i|
+        if i.is_a?(Numeric)
+            i * 2
+        else
+            i
+        end
+    end
     p result
 end
 
-arr =[1,2,3,4,5,]
+arr =[2,"a",3,4,"d","t",2.5]
 double_element(arr)
+
