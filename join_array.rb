749803a3c7667245647db9ad0185b26bef4cca65
Sanskar <sarathore@grepruby.io>
2025-09-24 11:17:25 +0530

    merge sorted array

--- join_array.rb
+++ join_array.rb
@@ -1,13 +1,32 @@
 def join_array(arr1,arr2)
     arr3 = []
-    arr1.each do |i|
-        arr3 << i
-    end
-    arr2.each do |i|
-        arr3 << i
-    end
+#     arr1.each do |i|
+#         arr3 << i
+#     end
+#     arr2.each do |i|
+#         arr3 << i
+#     end
+     i = 0
+     j = 0
+     while i < arr1.size && j < arr2.size
+        if arr1[i] < arr2[j]
+            arr3 << arr1[i]
+            i += 1
+        else
+            arr3 << arr2[j]
+            j += 1
+        end
+     end
+     while i < arr1.size
+        arr3 << arr1[i]
+        i += 1
+     end
+     while j < arr2.size
+        arr3 << arr[j]
+        j += 1
+     end
     p arr3
 end
-arr1 = [2,1,3,4]
-arr2 = [6,5,1]
+arr1 = [2,4]
+arr2 = [1,3]
 join_array(arr1,arr2)
